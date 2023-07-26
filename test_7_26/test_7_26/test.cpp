13577705c290b202334bbfc52635f9e3569b3651
TT982020 <xtli982020@outlook.com>
2023-07-26 20:51:11 +0800

    模板特化

--- test_7_26/test_7_26/test.cpp
+++ test_7_26/test_7_26/test.cpp
@@ -1,15 +1,44 @@
 #define _CRT_SECURE_NO_WARNINGS 1
-using namespace std;
+//using namespace std;
+//#include <iostream>
+//namespace x1 {
+//	int a = 10;
+//}
+//
+//namespace x2 {
+//	int a = 10;
+//}
+//
+//int main() {
+//	cout << x1::a << endl;
+//	cout << x2::a << endl;
+//}
 #include <iostream>
-namespace x1 {
-	int a = 10;
-}
 
-namespace x2 {
-	int a = 10;
-}
+template<class Key>
+struct hash
+{
+
+};
+template<>
+struct hash<char>
+{
+	size_t operator() (char c) { return c; }
+};
+
+template <>
+struct hash<int> {
+	size_t operator() (int i) { return i; }
+};
+
+template <>
+struct hash<long> {
+	size_t operator() (long l) { return l; }
+};
+
+
 
 int main() {
-	cout << x1::a << endl;
-	cout << x2::a << endl;
+	std::cout << hash<int>()(10) << std::endl;
+
 }
