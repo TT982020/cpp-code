b4a5c71c06ec30eeb3a66b8a32999264c446ed2a
TT982020 <xtli982020@outlook.com>
2023-10-05 18:48:36 +0800

    stack queue基本操作

---
+++ stl_stack_queue/stl_stack_queue/test.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <stack>
+#include <queue>
+using namespace std;
+void test_stack() {
+	stack<int> st;
+	st.push(1);
+	st.push(2);
+	st.push(3);
+	st.push(4);
+	while (!st.empty())
+	{
+		cout << st.top() << endl;
+		st.pop();
+	}
+}
+
+void test_queue() {
+	queue<int> q;
+	q.push(1);
+	q.push(2);
+	q.push(3);
+	q.push(4);
+	while (!q.empty())
+	{
+		cout << q.front() << endl;
+		q.pop();
+	}
+}
+
+int main() {
+	test_stack();
+	test_queue();
+	string s = "2";
+	atoi(s.c_str());
+}
